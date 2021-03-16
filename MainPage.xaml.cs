8b6ea1434516e17591f060f4b980014c10306799
Rilazy <35211613+Rilazy@users.noreply.github.com>
2021-03-16 17:10:39 -0600

    Hello, world for Win2D

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using System.Numerics;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +29,10 @@ namespace ChessAI
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args) {
+            args.DrawingSession.DrawText("Hello, World!", 100, 100, Colors.White);
         }
     }
 }
