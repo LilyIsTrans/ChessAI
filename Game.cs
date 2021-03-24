c19a063f2577a5ba04ddae2451b3ba3b831ec502
Rilazy <35211613+Rilazy@users.noreply.github.com>
2021-03-24 02:51:05 -0600

    Added board and uploaded assets

---
+++ Game.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessAI {
+
+    public struct Position : IEquatable<Position> {
+        public Position(byte row, byte column) {
+            nrow = row;
+            ncolumn = column;
+        }
+        private byte nrow;
+        private byte ncolumn;
+        public byte Row { 
+            get {
+                return nrow;
+            }
+            set {
+                if (value < 8) { nrow = value; }
+            }
+        }
+        public byte Column {
+            get {
+                return ncolumn;
+            }
+            set {
+                if (value < 8) { ncolumn = value; }
+            }
+        }
+
+        public override bool Equals(object obj) {
+            return obj is Position position && Equals(position);
+        }
+
+        public bool Equals(Position other) {
+            return (Column == other.Column) && (Row == other.Row);
+        }
+
+        public override int GetHashCode() {
+            int hashCode = 240067226;
+            hashCode = hashCode * -1521134295 + Row.GetHashCode();
+            hashCode = hashCode * -1521134295 + Column.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(Position left, Position right) {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Position left, Position right) {
+            return !(left == right);
+        }
+
+    }
+
+    class Game {
+        public const byte PAWN = 1, KNIGHT = 2, BISHOP = 3, ROOK = 4, QUEEN = 5, KING = 6;
+        public const bool WHITE = true, BLACK = false;
+        /* 1: Pawn
+         * 2: Knight
+         * 3: Bishop
+         * 4: Rook
+         * 5: Queen
+         * 6: King
+         */
+
+
+
+    }
+
+    abstract class Piece {
+
+        public Position position;
+        public bool white;
+        public static int type;
+        public byte Row {
+            get { return position.Row; }
+            set {
+                if (value < 8) { position.Row = value; }
+            }
+        }
+        public byte Column {
+            get { return position.Column; }
+            set {
+                if (value < 8) { position.Column = value; }
+            }
+        }
+
+        public abstract Position[] Moves();
+
+        public virtual Position[] Threaten() {
+            return Moves();
+        }
+
+
+    }
+
+    class Pawn : Piece {
+        new public static int type = Game.PAWN;
+
+        public Pawn(Position startPosition, bool isWhite) {
+            position = startPosition;
+            white = isWhite;
+        }
+
+        public override Position[] Moves() {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -16,6 +16,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Graphics.Canvas.Svg;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,18 +28,41 @@ namespace ChessAI
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        CanvasSvgDocument Board;
+        CanvasSvgDocument PawnBlack;
+        CanvasSvgDocument PawnWhite;
+        CanvasSvgDocument KnightBlack;
+        CanvasSvgDocument KnightWhite;
+        CanvasSvgDocument BishopBlack;
+        CanvasSvgDocument BishopWhite;
+        CanvasSvgDocument RookBlack;
+        CanvasSvgDocument RookWhite;
+        CanvasSvgDocument QueenBlack;
+        CanvasSvgDocument QueenWhite;
+        CanvasSvgDocument KingBlack;
+        CanvasSvgDocument KingWhite;
+
         public MainPage()
         {
             this.InitializeComponent();
         }
         // The actual renderer (currently Win2D)
         private void canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args) {
-            args.DrawingSession.DrawText("Yeet!", 100, 100, Colors.White);
+            //args.DrawingSession.DrawRectangle(0, 0, 100, 100, Color.FromArgb(100, 117, 52, 17));
+            Size BoardSize;
+            BoardSize.Width = 1800;
+            BoardSize.Height = 1800;
+            args.DrawingSession.DrawSvg(Board, sender.Size);
         }
 
         private void Page_Unloaded(object sender, RoutedEventArgs e) {
             this.canvas.RemoveFromVisualTree();
             this.canvas = null;
+        }
+
+        private void canvas_CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args) {
+            var file = File.ReadAllText("Assets/Chess Board.svg");
+            Board = CanvasSvgDocument.LoadFromXml(sender, file);
         }
     }
 }
